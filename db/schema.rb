c9d8013b3b2efd024b8b186241bdec3b527e8e7e
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-04-21 10:15:39 +1000

    added pairing table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115210308) do
+ActiveRecord::Schema.define(version: 20180421001347) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "recipient_id"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20161115210308) do
     t.datetime "updated_at",      null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "pairings", force: :cascade do |t|
+    t.integer  "user1"
+    t.integer  "user2"
+    t.boolean  "user1reveal"
+    t.boolean  "user2reveal"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
