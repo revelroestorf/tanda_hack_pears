23dfb5ad62a1d12a36985ccc2b65ea0a7e5bd666
Jordan Geltch-Robb <jgeltchrobb@hotmail.com>
2018-04-21 11:58:34 +1000

    reveal controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'home#index'
-
   devise_for :users
-
+  resources :reveal
+  
   resources :conversations, only: [:create] do
     member do
       post :close
     end
-
+    
     resources :messages, only: [:create]
   end
 end
