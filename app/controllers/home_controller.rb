7e3cf59646f8f1d5f3ee18e057c4b22d70bed897
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-04-21 12:00:27 +1000

    can only talk one person

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,15 @@ class HomeController < ApplicationController
   def index
     session[:conversations] ||= []
 
-    @users = User.all.where.not(id: current_user)
+    @pair = Pairing.where(user1:current_user.id).or(Pairing.where(user2:current_user.id)).first
+    # chat_pair_id = pair.user1 if pair.user1 != current_user.id
+    # chat_pair_id = pair.user2 if pair.user2 != current_user.id
+    
+    pair.user1 != current_user.id ? chat_partner_id = pair.user1 : chat_partner_id = pair.user2
+    
+    @user = User.find(chat_partner_id)
+
+     
     @conversations = Conversation.includes(:recipient, :messages)
                                  .find(session[:conversations])
   end
